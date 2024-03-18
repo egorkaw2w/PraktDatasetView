e7aeb11011001c220c3de2a5bf0e790785631929
egorkaw2w <85741587+egorkaw2w@users.noreply.github.com>
2024-03-18 17:03:15 +0300

    Добавьте файлы проекта.

---
+++ DataSet2.cs
@@ -0,0 +1,15 @@
+﻿namespace PraktDatasetView
+{
+
+
+    partial class DataSet2
+    {
+    }
+}
+
+namespace PraktDatasetView.DataSet2TableAdapters {
+    
+    
+    public partial class EmployeeTableAdapter {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PraktDatasetView.DataSet2TableAdapters;
+
+namespace PraktDatasetView
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmployeeViewTableAdapter View = new EmployeeViewTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            GridInfo.ItemsSource = View.GetDataBy();
+            
+        }
+
+        private void GridInfo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+    }
+}
